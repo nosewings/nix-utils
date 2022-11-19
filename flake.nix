b3167ed247a3e672b6c19f6565860bb4ebf0131b
Nicholas Coltharp <coltharp@pdx.edu>
2022-11-19 04:37:59 -0600

    Initial commit
    Add `removeKey` and `mapAccumL`.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: let
+    inherit (nixpkgs.lib) elemAt filterAttrs length;
+  in {
+    removeKey = x: filterAttrs (y: _: y != x);
+    mapAccumL = f: e: xs: let
+      stop = builtins.length xs;
+      go = results: state: n:
+        if n == stop then
+          { inherit state results; }
+        else let
+          p = f state (elemAt xs n);
+        in go (results ++ [ p.result ]) p.state (n + 1);
+    in go [ ] e 0;
+  };
+}
