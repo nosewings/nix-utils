2630d9167c93183fb94ad19c76a2cf8a1e93861a
Nicholas Coltharp <coltharp@pdx.edu>
2022-11-23 17:51:45 -0600

    Order functions alphabetically

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
   outputs = { self, nixpkgs }: let
     inherit (nixpkgs.lib) elemAt filterAttrs genList length min;
   in {
-    removeAttr = set: x: removeAttrs set [ x ];
     mapAccumL = f: e: xs: let
       stop = builtins.length xs;
       go = results: state: n:
@@ -18,6 +17,7 @@
           p = f state (elemAt xs n);
         in go (results ++ [ p.result ]) p.state (n + 1);
     in go [ ] e 0;
+    removeAttr = set: x: removeAttrs set [ x ];
     zipWith = f: xs: ys: genList (i: f (elemAt xs i) (elemAt ys i)) (min (length xs) (length ys));
   };
 }
